239b8d89b6c8dbcc7b44ecc6a0a91f4fac07e974
inoue-yuki <akki.seat@gmail.com>
2015-02-20 16:38:20 +0100

    Deleted unnecessary folder

--- Presenter/Community.php
+++ Presenter/Community.php
@@ -1,27 +1,18 @@
 <?php
 namespace Presenter;
 use Vendor\Pattern as Pattern;
-<<<<<<< HEAD
 use Vendor;
-=======
->>>>>>> 3506036... Added files
 use Model;
 
 class Community extends Pattern
 {
 	private $community;
-<<<<<<< HEAD
 	private $system;
-=======
->>>>>>> 3506036... Added files
 
 	public function start()
 	{
 		$this->community = new Model\Community;
-<<<<<<< HEAD
 		$this->system = new Vendor\System;
-=======
->>>>>>> 3506036... Added files
 	}
 
 	public function renderShow()
@@ -31,11 +22,8 @@ class Community extends Pattern
 		if( !empty( $community ) ) {
 			$this->data["group"] = $community;
 			$this->renderView("community/show");
-<<<<<<< HEAD
 		} else {
 			$this->system->error404();
-=======
->>>>>>> 3506036... Added files
 		}
 	}
 }
